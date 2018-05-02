f0227daf57ef7b018a64f0dd9a2d05de703327f9
Bailee Sjostrom <20sjobai@student.mcpsmt.org>
2018-05-02 10:15:14 -0600

    need to pull parent story

--- app/src/main/java/org/pltw/examples/storytime/RandomStory.java
+++ app/src/main/java/org/pltw/examples/storytime/RandomStory.java
@@ -1,6 +1,13 @@
 package org.pltw.examples.storytime;
 
-public class RandomStory {
+import java.util.Random;
+
+public class RandomStory extends ParentStory{
+    public final String TAG = this.getClass().getName();
+
+    Random rand = new Random();
+    int n = rand.nextInt(8);
+    //switch(randomEvent)
 
 
 }
