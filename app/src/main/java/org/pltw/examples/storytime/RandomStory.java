94a926e037324a5715db6be2d1906a717e7ff5bd
Bailee Sjostrom <20sjobai@student.mcpsmt.org>
2018-05-02 10:32:45 -0600

    pull Lande code

--- app/src/main/java/org/pltw/examples/storytime/RandomStory.java
+++ app/src/main/java/org/pltw/examples/storytime/RandomStory.java
@@ -6,8 +6,32 @@ public class RandomStory extends ParentStory{
     public final String TAG = this.getClass().getName();
 
     Random rand = new Random();
+
     int n = rand.nextInt(8);
-    //switch(randomEvent)
+    String storyString;
+
+   /* switch (n) {
+        case 1: storyString = "Story about Wolf";
+            break;
+        case 2:  storyString = "Story about Steve";
+            break;
+        case 3:  storyString = "Story about Hunger";
+            break;
+        case 4:  storyString = "Story about Village Friend";
+            break;
+        case 5:  storyString = "Story about Insanity Levels";
+            break;
+        case 6:  storyString = "Story about TreeEvent";
+            break;
+        case 7:  storyString = "Story about Intelligence";
+            break;
+        default: storyString = "Invalid story";
+            break;
+    }
+        System.out.println(storyString);
+    }
+} */
 
 
-}
+
+
