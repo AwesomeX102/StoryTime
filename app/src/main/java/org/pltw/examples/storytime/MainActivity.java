33cdf01cecfee43d3c99616fb1009bf927057e0c
AwesomeX102 <18laulor@student.mcpsmt.org>
2018-05-02 09:45:41 -0600

    Made a change

--- app/src/main/java/org/pltw/examples/storytime/MainActivity.java
+++ app/src/main/java/org/pltw/examples/storytime/MainActivity.java
@@ -5,34 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private Typewriter mainText;
+    private TextView mainText;
     private TextView textOne;
     private TextView textTwo;
     private TextView textThree;
     private String TAG = this.getClass().getName();
     //This was loren
 
-    /*private void textEffect(TextView textBox, String string){
-        for(int i = 0; i < string.length(); i++){
-            String finalString = "";
-            String x = string.substring(i);
-            try {
-                synchronized (this) {
-                    wait(250);
-                }
-            } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
-                Log.d(TAG, "Waiting didnt work!!");
-                e.printStackTrace();
-            }
-            finalString += x;
-            textBox.setText(finalString);
 
 
-        }
-        textBox.
 
-    }*/
 
 
 
@@ -42,13 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
-        Typewriter writer = new Typewriter(this);
-        setContentView(writer);
-
-        //Add a character every 150ms
-        writer.setCharacterDelay(150);
-        writer.animateText("Sample String");
+        
 
 
     }
